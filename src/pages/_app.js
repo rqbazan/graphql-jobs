5e5ad5fd1852465c27fb61e722f34674089628d6
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-18 15:28:51 -0500

    chore: remove unexpected compiled files

--- src/pages/_app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("../global.css");
-var app_1 = require("next/app");
-exports["default"] = app_1["default"];
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-exports["default"] = (function () { return <div className="p-4 bg-red-400">hello there</div>; });
