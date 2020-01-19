ace0d571fa839e0b2dcb2b116808116b8b5a8ef9
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-19 15:14:35 -0500

    feat: add withApollo hoc

---
+++ src/hocs/with-apollo.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import Head from 'next/head'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import fetch from 'isomorphic-unfetch'
+
+let cachedApolloClient = null
+
+function createApolloClient(initialState = {}) {
+  const httpLink = createHttpLink({
+    fetch,
+    uri: process.env.API_URL,
+    credentials: 'same-origin'
+  })
+
+  const cache = new InMemoryCache().restore(initialState)
+
+  return new ApolloClient({
+    cache,
+    ssrMode: typeof window === 'undefined',
+    link: httpLink
+  })
+}
+
+function initApolloClient(initialState) {
+  if (typeof window === 'undefined') {
+    return createApolloClient(initialState)
+  }
+
+  if (!cachedApolloClient) {
+    cachedApolloClient = createApolloClient(initialState)
+  }
+
+  return cachedApolloClient
+}
+
+export default function withApollo(PageComponent, { ssr = true } = {}) {
+  const WithApollo = ({ apolloClient, apolloState, ...pageProps }) => {
+    const client = apolloClient || initApolloClient(apolloState)
+    return (
+      <ApolloProvider client={client}>
+        <PageComponent {...pageProps} />
+      </ApolloProvider>
+    )
+  }
+
+  // Set the correct displayName in development
+  if (process.env.NODE_ENV !== 'production') {
+    const displayName =
+      PageComponent.displayName || PageComponent.name || 'Component'
+
+    if (displayName === 'App') {
+      console.warn('This withApollo HOC only works with PageComponents.')
+    }
+
+    WithApollo.displayName = `withApollo(${displayName})`
+  }
+
+  if (ssr || PageComponent.getInitialProps) {
+    WithApollo.getInitialProps = async ctx => {
+      const { AppTree } = ctx
+
+      const apolloClient = initApolloClient({})
+      ctx.apolloClient = apolloClient
+
+      // Run wrapped getInitialProps methods
+      let pageProps = {}
+
+      if (PageComponent.getInitialProps) {
+        pageProps = await PageComponent.getInitialProps(ctx)
+      }
+
+      // Only on the server:
+      if (typeof window === 'undefined') {
+        // When redirecting, the response is finished.
+        // No point in continuing to render
+        if (ctx.res && ctx.res.finished) {
+          return pageProps
+        }
+
+        // Only if ssr is enabled
+        if (ssr) {
+          try {
+            // Run all GraphQL queries
+            const { getDataFromTree } = await import('@apollo/react-ssr')
+            await getDataFromTree(
+              <AppTree
+                pageProps={{
+                  ...pageProps,
+                  apolloClient
+                }}
+              />
+            )
+          } catch (error) {
+            // Prevent Apollo Client GraphQL errors from crashing SSR.
+            // Handle them in components via the data.error prop:
+            // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
+            console.error('Error while running `getDataFromTree`', error)
+          }
+
+          // getDataFromTree does not call componentWillUnmount
+          // head side effect therefore need to be cleared manually
+          Head.rewind()
+        }
+      }
+
+      // Extract query data from the Apollo store
+      const apolloState = apolloClient.cache.extract()
+
+      return {
+        ...pageProps,
+        apolloState
+      }
+    }
+  }
+
+  return WithApollo
+}
