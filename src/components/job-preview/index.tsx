f63b70de02a5fdcceebf15cc07803cb3834b0dd9
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-19 02:11:37 -0500

    refactor: remove unnecessary styles.css
    - Prefer tailwind on markup
    - Set font-medium for job-preview--title

--- src/components/job-preview/index.tsx
+++ src/components/job-preview/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import cs from 'classnames'
-import './styles.css'
 
 export interface JobPreviewProps {
   className?: string
@@ -11,7 +10,9 @@ const JobPreview: React.FC<JobPreviewProps> = ({ className }) => {
     <div className={cs('flex rounded-xl bg-white p-4 shadow', className)}>
       <div className="mr-2 rounded-1/2 bg-red-400 h-12 w-12" />
       <div className="flex flex-col justify-between">
-        <div className="text-gray-800">Senior Fullstack Engineer</div>
+        <div className="text-gray-800 font-medium">
+          Senior Fullstack Engineer
+        </div>
         <div className="flex text-gray-600 text-xs items-center">
           <span className="mr-4">Segment</span>
           <span className="mr-4 w-8 bg-gray-600" style={{ height: 1 }} />
