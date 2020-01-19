5261b6dd4f077ec031bb92acd4354b3b727c98d8
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-19 03:22:00 -0500

    fix: add sort button to search bar desktop

--- src/components/search-bar/index.desktop.tsx
+++ src/components/search-bar/index.desktop.tsx
@@ -11,23 +11,31 @@ export interface SearchBarMobileProps {
 const SearchBarDesktopProps: React.FC<SearchBarMobileProps> = () => {
   return (
     <div className="flex">
-      <Input
-        className="w-1/2 rounded-r-none z-10"
-        icon={<Icon name="search" />}
-      />
-      <Select
-        prompt="Country"
-        className="w-1/4 rounded-t-none rounded-b-none border-r border-l border-gray-300"
+      <div className="flex-1 flex">
+        <Input
+          className="w-1/2 rounded-r-none z-10"
+          icon={<Icon name="search" />}
+        />
+        <Select
+          prompt="Country"
+          className="w-1/4 rounded-t-none rounded-b-none border-r border-l border-gray-300"
+        >
+          <option>USA</option>
+          <option>Germany</option>
+          <option>China</option>
+        </Select>
+        <Select prompt="Company" className="w-1/4 rounded-l-none z-10">
+          <option>Facebook</option>
+          <option>Google</option>
+          <option>Segment</option>
+        </Select>
+      </div>
+      <div
+        role="button"
+        className="flex items-center justify-center bg-white rounded-1/2 h-10 w-10 ml-4 shadow"
       >
-        <option>USA</option>
-        <option>Germany</option>
-        <option>China</option>
-      </Select>
-      <Select prompt="Company" className="w-1/4 rounded-l-none z-10">
-        <option>Facebook</option>
-        <option>Google</option>
-        <option>Segment</option>
-      </Select>
+        <Icon name="sort" />
+      </div>
     </div>
   )
 }
