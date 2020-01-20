ff67b36a8648164459769dee6f1706f70234ef0b
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-20 03:10:19 -0500

    refactor: include getCacheKey on Context type

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@ import { NormalizedCacheObject, InMemoryCache } from 'apollo-cache-inmemory'
 export interface Context {
   client: ApolloClient<NormalizedCacheObject>
   cache: InMemoryCache
+  getCacheKey: (obj: { __typename: string; id: string | number }) => string
 }
 
 export interface Resolver<R, A = { [key: string]: any }, P = {}> {
